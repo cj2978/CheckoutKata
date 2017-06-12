6f93880bc6415dfb6bb0f9d6cbe7c3adf9d7cc02
cj2978 <chris@thejonesathome.co.uk>
2017-06-12 12:24:23 +0100

    Initial Service Setup
    Creation of the Checkout WCF Service to be used in the Technical Test

---
+++ CheckoutService/CheckoutService/Checkout.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CheckoutService
+{
+    public class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutService/CheckoutService/ICheckout.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace CheckoutService
+{
+    [ServiceContract]
+    public interface ICheckout
+    {
+
+        [OperationContract]
+        void Scan(string item);
+
+        [OperationContract]
+        int GetTotalPrice();
+    }
+}
