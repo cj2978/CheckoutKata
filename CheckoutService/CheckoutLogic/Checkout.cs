a7c43d73b13f5aa42181d660e1a2e9ea4a33926a
cj2978 <chris@thejonesathome.co.uk>
2017-06-12 12:55:46 +0100

    JSON Reading
    Addition of funtionality to read the price list from the Prices.JSON file.

--- CheckoutService/CheckoutLogic/Checkout.cs
+++ CheckoutService/CheckoutLogic/Checkout.cs
@@ -1,4 +1,8 @@
-﻿namespace CheckoutLogic
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CheckoutLogic
 {
     public static class Checkout
     {
@@ -9,6 +13,18 @@
         public static int GetTotal()
         {
             return 0;
+        }
+
+        private static List<Price> GetPriceList()
+        {
+            List<Price> priceList = new List<Price>();
+
+            using (StreamReader reader = new StreamReader("prices.json"))
+            {
+                priceList = JsonConvert.DeserializeObject<List<Price>>(reader.ReadToEnd());
+            }
+
+            return priceList;
         }
     }
 }
