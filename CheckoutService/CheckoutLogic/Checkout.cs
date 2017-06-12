d61706c4a05fabba03a79910f2bc002e5689c8eb
cj2978 <chris@thejonesathome.co.uk>
2017-06-12 13:03:36 +0100

    Addition Of Scan Logic
    Addition of functionality to add the items passed in to the running total.

--- CheckoutService/CheckoutLogic/Checkout.cs
+++ CheckoutService/CheckoutLogic/Checkout.cs
@@ -6,8 +6,23 @@ namespace CheckoutLogic
 {
     public static class Checkout
     {
-        public static void ScanItem(string items)
+        public static Dictionary<char, int> ITEMS = new Dictionary<char, int>();
+
+        public static void ScanItem(string item)
         {
+            foreach (char character in item.ToCharArray())
+            {
+                if (ITEMS.ContainsKey(character))
+                {
+                    var currentItems = ITEMS[character];
+                    currentItems++;
+                    ITEMS[character] = currentItems;
+                }
+                else
+                {
+                    ITEMS.Add(character, 1);
+                }
+            }
         }
 
         public static int GetTotal()
